aac423e6d5269af3839c7915360380a037fffe99
ilyasahmed2007-oss <ilyasahmed2007@gmail.com>
2026-06-05 00:32:00 +0500

    Update main.dart

--- form_app/lib/main.dart
+++ form_app/lib/main.dart
@@ -1,112 +1,83 @@
-// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-import 'src/autofill.dart';
-import 'src/form_widgets.dart';
-import 'src/http/mock_client.dart';
-import 'src/sign_in_http.dart';
-import 'src/validation.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
 
 void main() {
-  // Removed setupWindow() call
-  runApp(const FormApp());
+  runApp(const MyApp());
 }
 
-final demos = [
-  Demo(
-    name: 'Sign in with HTTP',
-    route: 'signin_http',
-    builder: (context) => SignInHttpDemo(
-      // This sample uses a mock HTTP client.
-      httpClient: mockClient,
-    ),
-  ),
-  Demo(
-    name: 'Autofill',
-    route: 'autofill',
-    builder: (context) => const AutofillDemo(),
-  ),
-  Demo(
-    name: 'Form widgets',
-    route: 'form_widgets',
-    builder: (context) => const FormWidgetsDemo(),
-  ),
-  Demo(
-    name: 'Validation',
-    route: 'validation',
-    builder: (context) => const FormValidationDemo(),
-  ),
-];
-
-final router = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomePage(),
-      routes: [
-        for (final demo in demos)
-          GoRoute(
-            path: demo.route,
-            builder: (context, state) => demo.builder(context),
-          ),
-      ],
-    ),
-  ],
-);
-
-class FormApp extends StatelessWidget {
-  const FormApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Form Samples',
-      theme: ThemeData(colorSchemeSeed: Colors.teal),
-      routerConfig: router,
+    return MaterialApp(
+      title: 'Organic Bloom Cosmetic',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const BillingPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class BillingPage extends StatefulWidget {
+  const BillingPage({super.key});
+  @override
+  State<BillingPage> createState() => _BillingPageState();
+}
+
+class _BillingPageState extends State<BillingPage> {
+  final InAppPurchase _iap = InAppPurchase.instance;
+  List<ProductDetails> _products = [];
+  bool _available = false;
+
+  final Set<String> _kIds = {
+    'monthly_plan',
+    'quarterly_plan',
+    'yearly_plan'
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    _available = await _iap.isAvailable();
+    if (!_available) return;
+    final ProductDetailsResponse response = await _iap.queryProductDetails(_kIds);
+    setState(() {
+      _products = response.productDetails;
+    });
+  }
+
+  Future<void> _buy(ProductDetails product) async {
+    final PurchaseParam purchaseParam = PurchaseParam(productDetails: product);
+    _iap.buyNonConsumable(purchaseParam: purchaseParam);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Form Samples')),
-      body: ListView(children: [...demos.map((d) => DemoTile(demo: d))]),
+      appBar: AppBar(title: const Text('Organic Bloom Cosmetic')),
+      body: _products.isEmpty
+         ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _products.length,
+              itemBuilder: (context, index) {
+                final product = _products[index];
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(product.title),
+                    subtitle: Text(product.description),
+                    trailing: ElevatedButton(
+                      onPressed: () => _buy(product),
+                      child: Text(product.price),
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
-}
-
-class DemoTile extends StatelessWidget {
-  final Demo? demo;
-
-  const DemoTile({this.demo, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(demo!.name),
-      onTap: () {
-        context.go('/${demo!.route}');
-      },
-    );
-  }
-}
-
-class Demo {
-  final String name;
-  final String route;
-  final WidgetBuilder builder;
-
-  const Demo({
-    required this.name,
-    required this.route,
-    required this.builder,
-  });
 }
